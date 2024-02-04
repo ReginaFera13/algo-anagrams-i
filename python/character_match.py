473c23c53ed9fe19b04941b7546f621efb778ecc
ReginaFera13 <shawnmariesmith@icloud.com>
2024-02-04 17:37:26 -0500

    solved

--- python/character_match.py
+++ python/character_match.py
@@ -2,11 +2,30 @@
 
 # Part 1
 def is_character_match(string1, string2):
-	# Your code here
-	pass
+    list1 = [*string1.replace(' ', '').lower()]
+    list2 = [*string2.replace(' ', '').lower()]
+    
+    if len(list1) != len(list2):
+        return False
+    else:
+        chars_found = 0
+        for char in list1:
+            if list2.index(char) >= 0:
+                chars_found += 1
+        if chars_found == len(list1):
+            return True
+        else:
+            return False
+            
 
 
 # Part 2
+list_of_words = ["threads", "trashed", "hardest", "hatreds", "hounds"]
 def anagrams_for(word, list_of_words):
-	# your code here
-	pass
+    anagrams = []
+    for curr in list_of_words:
+        if is_character_match(word, curr) == True:
+            anagrams.append(curr)
+    return anagrams
+
+print(anagrams_for("threads", list_of_words))
--- python/character_match_spec.py
+++ python/character_match_spec.py
@@ -1,17 +1,27 @@
 from character_match import is_character_match, anagrams_for
+import pytest
 
 # Part 1
-print(is_character_match('charm', 'march') == True)
-print(is_character_match('zach', 'attack') == False)
-print(is_character_match('CharM', 'mARcH') == True)
-print(is_character_match('abcde2', 'c2abed') == True)
+def test_is_character_match():
+    assert is_character_match('charm', 'march') == True
+    assert is_character_match('zach', 'attack') == False
+    assert is_character_match('CharM', 'mARcH') == True
+    assert is_character_match('abcde2', 'c2abed') == True
+    assert is_character_match('Anna Madrigal', 'A man and a girl') == True
 
-print("This test is for the challenge quesiton")
-print(is_character_match('Anna Madrigal', 'A man and a girl') == True)
+# print(is_character_match('charm', 'march') == True)
+# print(is_character_match('zach', 'attack') == False)
+# print(is_character_match('CharM', 'mARcH') == True)
+# print(is_character_match('abcde2', 'c2abed') == True)
+
+# print("This test is for the challenge quesiton")
+# print(is_character_match('Anna Madrigal', 'A man and a girl') == True)
 
 # Part 2
+def test_anagrams_for():
+    list_of_words = ["threads", "trashed", "hardest", "hatreds", "hounds"]
+    assert anagrams_for("threads", list_of_words) == ["threads", "trashed", "hardest", "hatreds"]
+    assert anagrams_for("apple", list_of_words) == []
 
-list_of_words = ["threads", "trashed", "hardest", "hatreds", "hounds"]
-
-print(anagrams_for("threads", list_of_words) == ["threads", "trashed", "hardest", "hatreds"])
-print(anagrams_for("apple", list_of_words) == [])
+# print(anagrams_for("threads", list_of_words) == ["threads", "trashed", "hardest", "hatreds"])
+# print(anagrams_for("apple", list_of_words) == [])
